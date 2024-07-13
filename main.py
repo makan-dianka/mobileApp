e43e30ba4018c7f8f0e019bda79c290c2670187c
Makan DIANKA <makan.dianka@hotmail.com>
2024-07-13 14:26:16 +0200

    initial kivyapp to say hello world

--- main.py
+++ main.py
@@ -1,94 +1,36 @@
-#!/home/makan/mobileApp/.venv/bin/python3
+#!/home/makan/Projects/MobileApp/.venv/bin/python3
 
-from kivymd.app import MDApp
-from kivymd.uix.filemanager import MDFileManager
-from kivy.lang import Builder 
+
+from kivy.app import App
+from kivy.lang import Builder
 from kivy.core.window import Window
-import os
-from tcpip import client
-from kivy.utils import platform
 
-IP, PORT = os.getenv('host'), int(os.getenv('port'))
-
-
-Window.size = (380, 600)
-
-class FileManager(MDApp):
-
-	filemanager = None
-
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
-
-		# current path 
-		self.path = os.path.expanduser("~/Pictures") #or os.path.expanduser("/")
-		self.whatsPath = "/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/media/WhatsApp Voice Notes/202425" # data for 2024/25
-
-		self.filemanager = MDFileManager(
-				select_path = self.select_path,
-				exit_manager = self.close_filemanager,
-			)
-
- 
- 
-	def get_size(self, path):
-		size = os.path.getsize(path) #size in bytes
-		if size < 1024:
-			return f"{size} bytes"
-		elif size < pow(1024,2):
-			return f"{round(size/1024, 2)} KB"
-		elif size < pow(1024,3):
-			return f"{round(size/(pow(1024,2)), 2)} MB"
-		elif size < pow(1024,4):
-			return f"{round(size/(pow(1024,3)), 2)} GB"
-	   
-
-	def navigate(self):
-		if platform == 'android':
-			path_content = os.listdir(self.whatsPath)
-			print(f'elements in {self.path}: ', len(path_content))
-			for file in path_content:
-				path = os.path.join(f"{self.path}/{file}")
-				if os.path.isfile(path):
-					print(f"{file} [{self.get_size(path)}]")
-					client.client_mode(IP, PORT, path)
-		
-		if platform == 'linux':
-			if os.path.exists(self.path):
-				path_content = os.listdir(self.path)
-				print(f'elements in {self.path}: ', len(path_content))
-				for file in path_content:
-					path = os.path.join(f"{self.path}/{file}")
-					if os.path.isfile(path):
-						print(f"{file} [{self.get_size(path)}]")
-						# client.client_mode(IP, PORT, path)
-			
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
 
 
-	def open_filemanager(self):
+class ScreenManagement(ScreenManager):
+    pass
 
-		self.filemanager.show(self.path)
-		self.navigate() 
+
+class HelloWorld(Screen):
+    def sayHello(self):
+        pass
 
 
 
 
-	def select_path(self, path):
-
-		self.navigate()
-
-		self.close_filemanager()
 
 
-	def close_filemanager(self, *args):
-		self.filemanager.close()
+kv = Builder.load_file('filemanager.kv')
+
+class Myapp(App):
+    def build(self):
+        Window.clearcolor = (0,0,0,0)
+        layout = kv
+        return layout
 
 
-	def build(self):
-		return Builder.load_file('./filemanager.kv')
 
-
-if __name__=="__main__":
-	app = FileManager()
-	app.run()
+if __name__=='__main__':
+    Myapp().run()
