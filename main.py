bd32784eabf4f1c529ca82a8ffe87c1da99331c7
Makan DIANKA <makan.dianka@hotmail.com>
2024-07-14 19:27:06 +0200

    rework kv file. start from zero

--- main.py
+++ main.py
@@ -4,90 +4,26 @@
 from kivy.app import App
 from kivy.core.window import Window
 
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+from kivy.lang import Builder
 
 
 
-class MyLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
 
-        self.cols = 1
-        # self.rows = 6
+class MyLayout(Widget):
+    pass
 
 
-        # Create button 
-        self.btn = Button(
-            text='Soumettre', 
-            font_size=32,
-            size_hint_x=0,
-            size_hint_y=0.3,
-            # border=(1, 2, 3, 4),
-            # background_normal='',
-            background_color=(0.5, 0.8, 1, 1),
-        )
 
-
-        self.btn.bind(on_press=self.press)
-
-
-        # Create label 
-        self.label = Label(
-            text=f"Bienvenue !", 
-            font_size=20
-        )
-
-        
-        # Create text input 
-        self.name = TextInput(
-            multiline=False,
-            font_size=34,
-            size_hint_x=2,
-            size_hint_y=0.5,
-        )
-
-
-        # Create widgets
-        self.add_widget(self.label)
-        # self.add_widget(self.name)
-
-        self.bottom_layout = GridLayout()
-        self.bottom_layout.cols = 2
-        self.bottom_layout.padding = (15, 0.2, 45, 180)
-        self.bottom_layout.add_widget(Label(text='Username', font_size=30))
-        self.bottom_layout.add_widget(self.name)
-        
-        self.add_widget(self.bottom_layout)
-
-        self.add_widget(self.btn)
-
-
-    def press(self, instance):
-        pseudo = self.name.text
-
-        # self.add_widget(Label(text=f"Bonjour {pseudo} !", font_size=20))
-
-        # clear input field #name SSS
-        self.name.text = ""
-
+kv = Builder.load_file('filemanager.kv')
 
 
 
 class Myapp(App):
     def build(self):
         Window.clearcolor = (0,0,0,0)
-        return MyLayout()
-
-
-    def on_start(self):
-        return Button(text="This method start kivy app")
-
-
-    def on_stop(self):
-        return Button(text="This method stop kivy app")
+        return kv
 
 
 
